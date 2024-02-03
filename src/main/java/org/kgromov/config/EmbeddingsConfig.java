34fc95839760ab158ffdc0dc46a8f23d0ec2cc06
konstantin <konst.gromov@gmail.com>
2024-02-04 00:59:46 +0200

    Add inmemory embedding store

--- src/main/java/org/kgromov/config/EmbeddingsConfig.java
+++ src/main/java/org/kgromov/config/EmbeddingsConfig.java
@@ -2,18 +2,22 @@ package org.kgromov.config;
 
 import dev.langchain4j.chain.ConversationalRetrievalChain;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.retriever.EmbeddingStoreRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.cassandra.AstraDbEmbeddingConfiguration;
 import dev.langchain4j.store.embedding.cassandra.AstraDbEmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @RequiredArgsConstructor
@@ -40,12 +44,19 @@ public class EmbeddingsConfig {
     }
 
     @Bean
-    public AstraDbEmbeddingStore embeddingStore(AstraDbEmbeddingConfiguration embeddingConfiguration) {
+    public EmbeddingStore<TextSegment> embeddingStore(AstraDbEmbeddingConfiguration embeddingConfiguration) {
         return new AstraDbEmbeddingStore(embeddingConfiguration);
     }
 
+
+    @Profile("test")
     @Bean
-    public EmbeddingStoreIngestor embeddingStoreIngestor(AstraDbEmbeddingStore embeddingStore, EmbeddingModel embeddingModel) {
+    public EmbeddingStore<TextSegment> embeddingStore() {
+        return new InMemoryEmbeddingStore<>();
+    }
+
+    @Bean
+    public EmbeddingStoreIngestor embeddingStoreIngestor(EmbeddingStore<TextSegment> embeddingStore, EmbeddingModel embeddingModel) {
         return EmbeddingStoreIngestor.builder()
                 .documentSplitter(DocumentSplitters.recursive(300, 0))
                 .embeddingModel(embeddingModel)
@@ -54,7 +65,7 @@ public class EmbeddingsConfig {
     }
 
     @Bean
-    public ConversationalRetrievalChain conversationalRetrievalChain(AstraDbEmbeddingStore embeddingStore,
+    public ConversationalRetrievalChain conversationalRetrievalChain(EmbeddingStore<TextSegment> embeddingStore,
                                                                      EmbeddingModel embeddingModel) {
         return ConversationalRetrievalChain.builder()
                 .chatLanguageModel(OpenAiChatModel.withApiKey(openai.apiKey()))
